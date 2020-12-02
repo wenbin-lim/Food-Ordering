0bd3052a3625d9699b375a5867907d2cb9d41327
wenbin <wenbinlim93@gmail.com>
2020-12-02 17:56:23 +0800

    above

--- middleware/auth.js
+++ middleware/auth.js
@@ -63,9 +63,10 @@ module.exports = (privateOnly, minAccessLevel = customerAccess) => {
           return res.status(401).send('Unauthorized');
         }
 
-        if (foundBill?.status && foundBill.status === billStatus.settled) {
-          return res.status(401).send('Unauthorized');
-        }
+        /* temp removal */
+        // if (foundBill?.status && foundBill.status === billStatus.settled) {
+        //   return res.status(401).send('Unauthorized');
+        // }
 
         req.company = companyId;
         req.access = customerAccess;
